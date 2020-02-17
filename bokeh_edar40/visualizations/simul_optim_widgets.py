5da1560b6d97697c5f842ff77fd9cf2bff3a6a14
David Velasquez <dvelasquez@vicomtech.org>
2020-02-17 22:54:23 +0100

    Production ready

--- bokeh_edar40/visualizations/simul_optim_widgets.py
+++ bokeh_edar40/visualizations/simul_optim_widgets.py
@@ -251,8 +251,8 @@ class DynamicOptimWidget:
 		return dict_condicion
 
 def create_optim_div(target, possible_targets, var_influyentes, ranges):
-	endpoint = "http://10.0.20.30:9995/optimizacion"
-	# endpoint = "http://edar.vicomtech.org/optimizacion"
+	# endpoint = "http://10.0.20.30:9995/optimizacion"
+	endpoint = "http://edar.vicomtech.org/optimizacion"
 	data = {
 		'target': target,
 		'valores': possible_targets,
