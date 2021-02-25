c3b90e063054d1df5e8f44f60758ea6f56fe4415
David Velasquez <tidusdavid@gmail.com>
2021-02-25 10:04:22 +0100

    Remove date filter

--- parser_edar40/common/settings.py
+++ parser_edar40/common/settings.py
@@ -1,6 +1,6 @@
 # Date column filtering variables
 blnFilter_on_start_date = True
-blnFilter_on_end_date = True
+blnFilter_on_end_date = False
 
 # PERIOD 1 start and end dates [2013-2014]
 str_start_date_filter_PERIOD_1="2012-12-31" # For a condition >
